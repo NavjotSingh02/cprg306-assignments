5afcb04ab8b8ff08720893219fdfdf5c251b8199
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-09-12 14:53:27 -0600

    link and name

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function Student() {
+    return (
+      <main>
+         <h1>Navjot Singh</h1>
+         <Link href="https://github.com/NavjotSingh02/cprg306-assignments.git">Github Link</Link>
+         
+      </main>
+    );
+  }
+  
