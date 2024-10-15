c3b56c08981adf715c69841d1ab7524e0be977bf
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-10-14 19:57:10 -0600

    again

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,85 +1,42 @@
 "use client";
 import { useState } from 'react';
 
-export default function Page() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("produce");
+export default function NewItem() {
   const [quantity, setQuantity] = useState(1);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const item = { name, quantity, category };
-    console.log(item);
-    alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
-    setName("");
-    setCategory("produce");
-    setQuantity(1);
+  const increment = () => {
+    if(quantity < 20) {
+      setQuantity(quantity + 1);
+    }
   };
 
-  const newItems = (newQuantity) => {
-    if (newQuantity >= 1 && newQuantity <= 20) {
-      setQuantity(newQuantity);
+  const decrement = () => {
+   if(quantity > 1) {
+      setQuantity(quantity - 1);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input 
-            type="text" 
-            value={name} 
-            onChange={(e) => setName(e.target.value)} 
-            placeholder="Item Name"
-            required 
-          />
-        </div>
-        <div>
-          <div>
-            <div>
-              <span>{quantity}</span>
-              <div>
-                <button 
-                  type="button" 
-                  onClick={() => newItems(quantity - 1)} 
-                  disabled={quantity === 1}
-                >
-                  -
-                </button>
-                <button 
-                  type="button" 
-                  onClick={() => newItems(quantity + 1)} 
-                  disabled={quantity === 20}
-                >
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-          <select 
-            value={category} 
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option value="" disabled>Category</option>
-            <option value="produce">Produce</option>
-            <option value="dairy">Dairy</option>
-            <option value="bakery">Bakery</option>
-            <option value="meat">Meat</option>
-            <option value="frozen">Frozen Foods</option>
-            <option value="canned">Canned Goods</option>
-            <option value="dry">Dry Goods</option>
-            <option value="beverages">Beverages</option>
-            <option value="snacks">Snacks</option>
-            <option value="household">Household</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <button 
-          type="submit"
-        >
-          +
+    <div className="flex  justify-center mt-4">
+    <div className=" bg-white p-2 w-36">
+      <input type="text" className=" text-black mr-4 w-10" value={quantity} />
+      
+    <button 
+          onClick={decrement} 
+          disabled={quantity === 1} 
+          className={` text-white w-8 rounded mx-1 ${quantity === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+    >
+         -
         </button>
-      </form>
+        <button 
+          onClick={increment} 
+          disabled={quantity === 20} 
+          className={`w-12  py-2 rounded ${quantity === 20 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+        >
+         +
+        </button>
+      
+    </div>
     </div>
   );
 }
