9048d76329f33ab3fadc79d8017632e9d4e2aad7
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-10-27 22:41:24 -0600

    week7

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Student2() {
          <Link href="http://localhost:3000/week-4">week-4</Link><br/>
          <Link href="http://localhost:3000/week-5">week-5</Link><br/>
           <Link href="http://localhost:3000/week-6">week-6</Link><br/>
+          <Link href="http://localhost:3000/week-7">week-7</Link><br/>
       </main>
     );
   }
