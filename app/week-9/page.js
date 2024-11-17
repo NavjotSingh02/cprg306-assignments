eb746fd4c29ee50276b5cdf4f7590878224c5340
NavjotSingh02 <navjot.singh02@edu.sait.ca>
2024-11-17 14:00:05 -0700

    Update page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,27 +1,23 @@
-"use client"; // Mark this component as a client component
-
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation'; // Use next/navigation instead of next/router
+"use client";
 import { useUserAuth } from "../week-9/_utils/auth-context";
-
-export default function ShoppingListPage() {
-  const { user } = useUserAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push('/week-9'); // Redirect to the landing page if not logged in
-    }
-  }, [user, router]);
-
-  if (!user) {
-    return null; // Do not render the shopping list page if not logged in
+export default function Page() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const login = async() => {
+  await gitHubSignIn();
+  };
+  const logout = async() => {
+  await firebaseSignOut();
   }
-
   return (
-    <main>
-      <h1>Shopping List</h1>
-      {/* Render the shopping list here */}
-    </main>
+    <div>
+      <h1>Page</h1>
+      <div>
+        {user ? (
+          <button onClick={logout}>Sign Out</button>
+        ) : (
+          <button onClick={login}>Sign In</button>
+        )}
+    </div>
+  </div>
   );
 }
